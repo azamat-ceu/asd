2a5db079849e027aa5d2b68f6ec8fbac0c8a387f
1 <1@303-14>
2020-02-13 17:28:19 +0500

    ЗАПОЛНИЛ СТРАНЫ ИИХ КОДЫ

---
+++ VIN_LIB/VIN_LIB/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace VIN_LIB
+{
+    public class VIN
+    {
+        bool CheckVIN(string vin)
+        {
+            Regex vi = new Regex(@"[A-H,J-N,P,R-Z,\d]{17}");
+            if (!vi.IsMatch(vin)) return false;
+        }
+        string GetVINCountry(string vin)
+        {
+
+        }
+        int GetTransportYear(string vin)
+        {
+
+        }
+    }
+}
